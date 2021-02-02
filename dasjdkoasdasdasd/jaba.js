86156cba6f3d26e64a5cce2b4a3452fad32f6534
Kovichka <78442381+Kovichka@users.noreply.github.com>
2021-02-02 22:05:37 +0300

    Add files via upload

---
+++ dasjdkoasdasdasd/jaba.js
@@ -0,0 +1,14 @@
+
+
+
+window.addEventListener('load', function () {
+    let but1 = document.getElementById("but1");
+    let but2 = document.getElementById("but2");
+    but1.addEventListener('click', function (){
+        window.location.href = "https://koldyn.com/magiya/kak-stat-feej-vinks.html";
+        });
+    but2.addEventListener('click', function (){
+        document.body.style.backgroundColor = prompt('Choose color of youre life ', 'black');
+    });
+});
+	
